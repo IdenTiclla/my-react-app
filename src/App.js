863f5f6548fdf3b911f44bd24741668eac679609
unknown <iden.ticlla@gmail.com>
2021-03-31 10:46:53 -0400

    state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function Helloworld(props) {
-    console.log(props)
-    return (
-        <div id="hello">
-            <h3>{props.subtitle}</h3>
-            {props.mytext}
-        </div>
-    )
+
+
+class Helloworld extends Component {
+    constructor() {
+        super()
+        this.state = {
+            show: true
+        }
+        this.toggleShow = this.toggleShow.bind(this)
+    }
+    
+
+
+    toggleShow() {
+        this.setState({show: !this.state.show})
+    }
+
+    render() {
+        if (this.state.show) {
+            return (
+                <div id="hello">
+                    <h3>{this.props.subtitle}</h3>
+                        {this.props.mytext}
+
+                    <button onClick={this.toggleShow}>Toggle show</button>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h1>No hay elementos</h1>
+                    <button onClick={this.toggleShow}>Toggle show</button>
+                </div>
+            )
+        }
+    }
 }
 
 function App() {
@@ -19,6 +47,7 @@ function App() {
             <Helloworld mytext="Hello alex" subtitle="component 3"/>
         </div>
     )
+    
 }
 
 
